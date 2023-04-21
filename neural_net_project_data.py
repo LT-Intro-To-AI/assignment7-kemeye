b3beaf367ef4b130bb4e9d89badae9a855ca61b9
Kevin <kmeyering@cps.edu>
2023-04-21 10:37:47 -0500

    have to log out...

--- neural_net_project_data.py
+++ neural_net_project_data.py
@@ -11,15 +11,23 @@ def parse_line(line: str) -> Tuple[List[float], List[float]]:
     Returns:
         tuple of input list and output list
     """
+    wordRef = {"v-high","high","med","low","more","small","med","big","low","high"}
+    numRef =  {3,2,1,0,6,0,1,2,0,2}
+    numWordRef = {
+        "v-high": 3,
+    }
     tokens = line.split(",")
-    # print(tokens)
-    out = int(tokens[0])
-    # print(out)
-    output = [0 if out == 1 else 0.5 if out == 2 else 1]
-    # print(output)
+    print(tokens)
+    out = tokens[0]
+    print(out)
+    # output = [0 if out == 1 else 0.5 if out == 2 else 1]
+    for x in tokens:
+        if type(x) == str:
+            output = 
+    print(output)
 
     inpt = [float(x) for x in tokens[1:]]
-    # print(inpt)
+    print(inpt)
     return (inpt, output)
 
 
@@ -49,7 +57,7 @@ def normalize(data: List[Tuple[List[float], List[float]]]):
     return data
 
 
-with open("wine_data.txt", "r") as f:
+with open("car.txt", "r") as f:
     training_data = [parse_line(line) for line in f.readlines() if len(line) > 4]
 
 td = normalize(training_data)
@@ -57,8 +65,8 @@ td = normalize(training_data)
 # for line in td:
 #     print(line)
 
-nn = NeuralNet(13, 3, 1)
-nn.train(td, iters=1000, print_interval=1000, learning_rate=0.1)
+# nn = NeuralNet(13, 3, 1)
+# nn.train(td, iters=1000, print_interval=1000, learning_rate=0.1)
 
-for i in nn.test_with_expected(td):
-    print(f"desired: {i[1]}, actual: {i[2]}")
+# for i in nn.test_with_expected(td):
+#     print(f"desired: {i[1]}, actual: {i[2]}")
